212f2bb7f57dcb3f9de610ebe2685657be5d5009
subin131 <satyalsubin33@gmail.com>
2020-05-09 15:48:06 +0545

    This is another file

---
+++ Example.java
@@ -0,0 +1,10 @@
+public class Example{
+    public static void main(String []args){
+        int n=0,sum=0;
+        while(n<=5){
+            sum+=n;
+            System.out.printf("sum[%d]=%d\n",n,sum);
+            n++;
+        }
+    }
+}
